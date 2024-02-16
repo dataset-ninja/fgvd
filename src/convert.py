cc3ca1445ee8231ec18a113cba2febf9f1725d6a
peixed351 <peixed351@gmail.com>
2024-02-16 14:24:13 +0300

    add settings

--- src/convert.py
+++ src/convert.py
@@ -1,12 +1,15 @@
-import supervisely as sly
 import os
-from dataset_tools.convert import unpack_if_archive
-import src.settings as s
-from urllib.parse import unquote, urlparse
-from supervisely.io.fs import get_file_name, get_file_size
 import shutil
+import xml.etree.ElementTree as ET
+from urllib.parse import unquote, urlparse
 
+import supervisely as sly
+from dataset_tools.convert import unpack_if_archive
+from supervisely.io.fs import file_exists, get_file_name
 from tqdm import tqdm
+
+import src.settings as s
+
 
 def download_dataset(teamfiles_dir: str) -> str:
     """Use it for large datasets to convert them on the instance"""
@@ -29,7 +32,7 @@ def download_dataset(teamfiles_dir: str) -> str:
             total=fsize,
             unit="B",
             unit_scale=True,
-        ) as pbar:        
+        ) as pbar:
             api.file.download(team_id, teamfiles_path, local_path, progress_cb=pbar)
         dataset_path = unpack_if_archive(local_path)
 
@@ -57,7 +60,8 @@ def download_dataset(teamfiles_dir: str) -> str:
 
         dataset_path = storage_dir
     return dataset_path
-    
+
+
 def count_files(path, extension):
     count = 0
     for root, dirs, files in os.walk(path):
@@ -65,21 +69,101 @@ def count_files(path, extension):
             if file.endswith(extension):
                 count += 1
     return count
-    
+
+
 def convert_and_upload_supervisely_project(
     api: sly.Api, workspace_id: int, project_name: str
 ) -> sly.ProjectInfo:
     ### Function should read local dataset and upload it to Supervisely project, then return project info.###
-    raise NotImplementedError("The converter should be implemented manually.")
+    dataset_path = "/home/alex/DATASETS/TODO/IDD_FGVD"
+    images_folder = "images"
+    bboxes_folder = "annos"
+    batch_size = 30
+    images_ext = ".jpg"
+    bboxes_ext = ".xml"
 
-    # dataset_path = "/local/path/to/your/dataset" # general way
-    # dataset_path = download_dataset(teamfiles_dir) # for large datasets stored on instance
+    def create_ann(image_path):
+        labels = []
 
-    # ... some code here ...
+        ann_path = image_path.replace(images_folder, bboxes_folder).replace(images_ext, bboxes_ext)
 
-    # sly.logger.info('Deleting temporary app storage files...')
-    # shutil.rmtree(storage_dir)
+        if file_exists(ann_path):
+            tree = ET.parse(ann_path)
+            root = tree.getroot()
 
-    # return project
+            img_height = int(root.find(".//height").text)
+            img_wight = int(root.find(".//width").text)
 
+            all_objects = root.findall(".//object")
 
+            for curr_object in all_objects:
+                tags = []
+                all_name = curr_object.find(".//name").text
+                split_name = all_name.split("_")
+                class_name = split_name[0]
+                if len(split_name) == 3:
+                    model = sly.Tag(model_meta, value=split_name[2])
+                    manufacturer = sly.Tag(manufacturer_meta, value=split_name[1])
+                    tags.extend([manufacturer, model])
+                elif len(split_name) == 2:
+                    manufacturer = sly.Tag(manufacturer_meta, value=split_name[1])
+                    tags.append(manufacturer)
+                obj_class = meta.get_obj_class(class_name)
+                coords_xml = curr_object.findall(".//bndbox")
+                for curr_coord in coords_xml:
+                    left = float(curr_coord[0].text)
+                    top = float(curr_coord[1].text)
+                    right = float(curr_coord[2].text)
+                    bottom = float(curr_coord[3].text)
+
+                    rect = sly.Rectangle(
+                        left=int(left), top=int(top), right=int(right), bottom=int(bottom)
+                    )
+                    label = sly.Label(rect, obj_class, tags=tags)
+                    labels.append(label)
+
+        return sly.Annotation(img_size=(img_height, img_wight), labels=labels)
+
+    mini = sly.ObjClass("mini-bus", sly.Rectangle)
+    car = sly.ObjClass("car", sly.Rectangle)
+    autorickshaw = sly.ObjClass("autorickshaw", sly.Rectangle)
+    bus = sly.ObjClass("bus", sly.Rectangle)
+    scooter = sly.ObjClass("scooter", sly.Rectangle)
+    motorcycle = sly.ObjClass("motorcycle", sly.Rectangle)
+    truck = sly.ObjClass("truck", sly.Rectangle)
+
+    manufacturer_meta = sly.TagMeta("manufacturer", sly.TagValueType.ANY_STRING)
+    model_meta = sly.TagMeta("model", sly.TagValueType.ANY_STRING)
+
+    project = api.project.create(workspace_id, project_name, change_name_if_conflict=True)
+    meta = sly.ProjectMeta(
+        obj_classes=[mini, car, autorickshaw, bus, scooter, motorcycle, truck],
+        tag_metas=[manufacturer_meta, model_meta],
+    )
+    api.project.update_meta(project.id, meta.to_json())
+
+    for ds_name in os.listdir(dataset_path):
+
+        images_path = os.path.join(dataset_path, ds_name, images_folder)
+        bboxes_path = os.path.join(dataset_path, ds_name, bboxes_folder)
+
+        images_names = os.listdir(images_path)
+
+        dataset = api.dataset.create(project.id, ds_name, change_name_if_conflict=True)
+
+        progress = sly.Progress("Create dataset {}".format(ds_name), len(images_names))
+
+        for images_names_batch in sly.batched(images_names, batch_size=batch_size):
+            img_pathes_batch = [
+                os.path.join(images_path, image_name) for image_name in images_names_batch
+            ]
+
+            img_infos = api.image.upload_paths(dataset.id, images_names_batch, img_pathes_batch)
+            img_ids = [im_info.id for im_info in img_infos]
+
+            anns = [create_ann(image_path) for image_path in img_pathes_batch]
+            api.annotation.upload_anns(img_ids, anns)
+
+            progress.iters_done_report(len(images_names_batch))
+
+    return project
--- src/settings.py
+++ src/settings.py
@@ -13,57 +13,94 @@ from dataset_tools.templates import (
 ##################################
 # * Before uploading to instance #
 ##################################
-PROJECT_NAME: str = None
-PROJECT_NAME_FULL: str = None
+PROJECT_NAME: str = "FGVD"
+PROJECT_NAME_FULL: str = "FGVD: Fine-Grained Vehicle Detection Dataset"
 HIDE_DATASET = True  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
 ##################################
-LICENSE: License = None
-APPLICATIONS: List[Union[Industry, Domain, Research]] = None
-CATEGORY: Category = None
+LICENSE: License = License.CC_BY_4_0()
+APPLICATIONS: List[Union[Industry, Domain, Research]] = [Industry.Automotive()]
+CATEGORY: Category = Category.SelfDriving()
 
-CV_TASKS: List[CVTask] = None
-ANNOTATION_TYPES: List[AnnotationType] = None
+CV_TASKS: List[CVTask] = [
+    CVTask.ObjectDetection(),
+    CVTask.Identification(),
+]
+ANNOTATION_TYPES: List[AnnotationType] = [AnnotationType.ObjectDetection()]
 
-RELEASE_DATE: Optional[str] = None  # e.g. "YYYY-MM-DD"
+RELEASE_DATE: Optional[str] = "2022-12-08"  # e.g. "YYYY-MM-DD"
 if RELEASE_DATE is None:
     RELEASE_YEAR: int = None
 
-HOMEPAGE_URL: str = None
+HOMEPAGE_URL: str = "https://zenodo.org/records/7488960"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = None
+PREVIEW_IMAGE_ID: int = 14047885
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
-GITHUB_URL: str = None
+GITHUB_URL: str = "https://github.com/dataset-ninja/fgvd"
 # URL to GitHub repo on dataset ninja (e.g. "https://github.com/dataset-ninja/some-dataset")
 
 ##################################
 ### * Optional after uploading ###
 ##################################
-DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = None
+DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = (
+    "https://zenodo.org/records/7488960/files/IDD_FGVD.tar.gz?download=1"
+)
 # Optional link for downloading original dataset (e.g. "https://some.com/dataset/download")
 
-CLASS2COLOR: Optional[Dict[str, List[str]]] = None
+CLASS2COLOR: Optional[Dict[str, List[str]]] = {
+    "mini-bus": [230, 25, 75],
+    "car": [60, 180, 75],
+    "autorickshaw": [255, 225, 25],
+    "bus": [0, 130, 200],
+    "scooter": [245, 130, 48],
+    "motorcycle": [145, 30, 180],
+    "truck": [70, 240, 240],
+}
 # If specific colors for classes are needed, fill this dict (e.g. {"class1": [255, 0, 0], "class2": [0, 255, 0]})
 
 # If you have more than the one paper, put the most relatable link as the first element of the list
 # Use dict key to specify name for a button
-PAPER: Optional[Union[str, List[str], Dict[str, str]]] = None
+PAPER: Optional[Union[str, List[str], Dict[str, str]]] = "https://arxiv.org/pdf/2212.14569"
 BLOGPOST: Optional[Union[str, List[str], Dict[str, str]]] = None
-REPOSITORY: Optional[Union[str, List[str], Dict[str, str]]] = {"GitHub":"some_link_to_repo_if_exists"}
+REPOSITORY: Optional[Union[str, List[str], Dict[str, str]]] = {
+    "GitHub": "https://github.com/iHubData-Mobility/public-FGVD"
+}
 
 CITATION_URL: Optional[str] = None
-AUTHORS: Optional[List[str]] = None
-AUTHORS_CONTACTS: Optional[List[str]] = None
+AUTHORS: Optional[List[str]] = (
+    [
+        "Prafful Kumar Khoba",
+        "Chirag Parikh",
+        "Rohit Saluja",
+        "Ravi Kiran Sarvadevabhatla",
+        "C.V. Jawahar",
+    ],
+)
+AUTHORS_CONTACTS: Optional[List[str]] = [
+    "qiz228274@iitd.ac.in",
+    "chirag.parikh@students.iiit.ac.in",
+    "rohit.saluja@research.iiit.ac.in",
+    "ravi.kiran@iiit.ac.in",
+    "jawahar@iiit.ac.in",
+]
 
-ORGANIZATION_NAME: Optional[Union[str, List[str]]] = None
-ORGANIZATION_URL: Optional[Union[str, List[str]]] = None
+ORGANIZATION_NAME: Optional[Union[str, List[str]]] = [
+    "Indian Institute Of Technology, India",
+    "The International Institute of Information Technology, India",
+]
+ORGANIZATION_URL: Optional[Union[str, List[str]]] = [
+    "https://home.iitd.ac.in/",
+    "https://www.iiit.ac.in/",
+]
 
 # Set '__PRETEXT__' or '__POSTTEXT__' as a key with string value to add custom text. e.g. SLYTAGSPLIT = {'__POSTTEXT__':'some text}
-SLYTAGSPLIT: Optional[Dict[str, Union[List[str], str]]] = None
+SLYTAGSPLIT: Optional[Dict[str, Union[List[str], str]]] = {
+    "__POSTTEXT__": "Additionally, labels contain information about ***manufacturer*** and ***model*** of vehicle. Explore it in supervisely labeling tool"
+}
 TAGS: Optional[List[str]] = None
 
 
