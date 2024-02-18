b8c768efdf66ee5dc295873ced103d568aabe627
peixed351 <peixed351@gmail.com>
2024-02-18 17:37:59 +0300

    release

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "FGVD"
 PROJECT_NAME_FULL: str = "FGVD: Fine-Grained Vehicle Detection Dataset"
-HIDE_DATASET = True  # set False when 100% sure about repo quality
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
